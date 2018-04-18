7fc30b499d24c8f776822da341d78dbadec4b503
Kevin Shreve <kshreve@gmail.com>
2018-04-17 22:26:07 -0400

    auto

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,13 @@ import Anchor from 'components/Anchor';
 
 const Header = props => (
   <Fragment>
+    {/* language=SCSS */}
+    <style jsx>{`
+      header {
+        margin: 0 auto;
+      }
+    `}
+    </style>
     <header {...props}>
       <Anchor to="/" text="Home" />
     </header>
