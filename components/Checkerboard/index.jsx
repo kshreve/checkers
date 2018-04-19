227b1cf97622a00fc171ef5c9ad4c186a3e91671
Kevin Shreve <kshreve@gmail.com>
2018-04-19 08:23:59 -0400

    only make board once in constructor

--- components/Checkerboard/index.jsx
+++ components/Checkerboard/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-array-index-key */
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import Square from 'components/Checkerboard/Square';
@@ -14,27 +14,41 @@ const makeBoard = (size) => {
   />));
 };
 
-const Checkerboard = ({ size }) => (
-  <Fragment>
-    {/* language=SCSS */}
-    <style jsx>{`
-      .checkerboard {
-        display: grid;
-        grid-template-columns: repeat(${size}, 1fr);
-        margin: 0 auto 10px auto;
-        border: 2px solid ${gridLines};
-        max-width: 640px;
-      }
-    `}
-    </style>
-    <div className="checkerboard">
-      {makeBoard(size)}
-    </div>
-  </Fragment>
-);
-Checkerboard.displayName = 'Checkerboard';
-Checkerboard.propTypes = {
-  size: PropTypes.number.isRequired,
-};
+export default class Checkerboard extends Component {
+  static displayName = 'Checkerboard';
+  static propTypes = {
+    size: PropTypes.number.isRequired,
+  };
 
-export default Checkerboard;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checkerboard: makeBoard(props.size),
+    };
+  }
+
+  render() {
+    const { size } = this.props;
+    const { checkerboard } = this.state;
+
+    return (
+      <Fragment>
+        {/* language=SCSS */}
+        <style jsx>{`
+          .checkerboard {
+            display: grid;
+            grid-template-columns: repeat(${size}, 1fr);
+            margin: 0 auto 10px auto;
+            border: 2px solid ${gridLines};
+            max-width: 640px;
+          }
+        `}
+        </style>
+        <div className="checkerboard">
+          {checkerboard}
+        </div>
+      </Fragment>
+    );
+  }
+}
