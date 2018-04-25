df5b59e954544da28598b1029d78a214755a5c73
Kevin Shreve <kshreve@gmail.com>
2018-04-25 09:04:16 -0400

    prepping for dragging

--- components/Checkerboard/Checker.jsx
+++ components/Checkerboard/Checker.jsx
@@ -1,41 +1,50 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { primaryColor, secondaryColor } from '../../assets/colors';
 
-import { primaryColor, secondaryColor } from 'assets/colors';
+class Checker extends Component {
+  static displayName = 'Checker';
+  static propTypes = {
+    row: PropTypes.number.isRequired,
+    column: PropTypes.number.isRequired,
+    firstPlayer: PropTypes.bool.isRequired,
+    secondPlayer: PropTypes.bool.isRequired,
+  };
 
+  render() {
+    const {
+      row, column, firstPlayer, secondPlayer,
+    } = this.props;
 
-const Checker = ({
-  row, column, firstPlayer, secondPlayer,
-}) => (
-  <Fragment>
-    {/* language=SCSS */}
-    <style jsx>{`
-      .checker {
-        border-color: white;
-        border-style: solid;
-        border-width: medium;
-        border-radius: 100px;
-        grid-column: ${column + 1};
-        grid-row: ${row + 1};
+    let backgroundColor = null;
+    if (firstPlayer) {
+      backgroundColor = primaryColor;
+    } else if (secondPlayer) {
+      backgroundColor = secondaryColor;
+    }
 
-        background-color: ${firstPlayer && primaryColor};
-        background-color: ${secondPlayer && secondaryColor};
-      }
-    `}
-    </style>
-    <div className="checker" />
-  </Fragment>
-);
-Checker.displayName = 'Checker';
-Checker.defaultProps = {
-  firstPlayer: true,
-  secondPlayer: false,
-};
-Checker.propTypes = {
-  row: PropTypes.number.isRequired,
-  column: PropTypes.number.isRequired,
-  firstPlayer: PropTypes.bool,
-  secondPlayer: PropTypes.bool,
-};
+    return (
+      <Fragment>
+        {/* language=SCSS */}
+        <style jsx>{`
+          .checker {
+            border-color: white;
+            border-style: solid;
+            border-width: medium;
+            border-radius: 100px;
+            grid-column: ${column + 1};
+            grid-row: ${row + 1};
+
+            background-color: ${backgroundColor};
+          }
+        `}
+        </style>
+        <div className="checker">
+          &nbsp;
+        </div>
+      </Fragment>
+    );
+  }
+}
 
 export default Checker;
