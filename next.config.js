72e0e59398d9837a467e294476b6a2402e90ae5a
Kevin Shreve <kshreve@gmail.com>
2018-05-21 22:05:03 -0400

    static export?

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 // next.config.js
 const withImages = require('next-images');
+const glob = require('glob');
 
-module.exports = withImages();
+module.exports = withImages({
+  exportPathMap: () => {
+    const pathMap = {};
+    glob.sync('pages/**/*.js', { ignore: 'pages/_document.js' }).forEach((s) => {
+      const path = s.split(/(pages|\.)/)[2].replace(/^\/index$/, '/');
+      pathMap[path] = { page: path };
+    });
+    return pathMap;
+  },
+});
